6e08eafc77aa9c37c3322013aff44992ea80e6b0
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-23 22:40:13 +0530

    Added Test Case with Mockito

--- MockitoJunitTesting/src/test/java/com/mockitotutorial/happyhotel/booking/BookingServiceTest.java
+++ MockitoJunitTesting/src/test/java/com/mockitotutorial/happyhotel/booking/BookingServiceTest.java
@@ -1,19 +1,29 @@
 package com.mockitotutorial.happyhotel.booking;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.awt.print.Book;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(MockitoJUnitRunner.class)
 class BookingServiceTest {
 
     @Mock
     PaymentService paymentService;
 
-    @Mock RoomService roomService;
+    @Mock
+    RoomService roomService;
+
+    private RoomService roomService3;
 
     @Mock
     BookingDAO bookingDAO;
@@ -30,5 +40,19 @@ class BookingServiceTest {
         String expected = "Welcome Rahman";
         String actual = bookingService1.welcomeMessage("Rahman");
         assertEquals(expected,actual);
+    }
+
+    @Test
+    void testCaseFor_getAvailablePlaceCount(){
+        ArrayList room = new ArrayList<>();
+        room.add(new Room("1",3));
+        room.add(new Room("2",6));
+        room.add(new Room("3",9));
+        this.roomService3 = mock(RoomService.class);
+        when(roomService3.getAvailableRooms()).thenReturn(room);
+        BookingService bookingService1 = new BookingService(paymentService,roomService3,bookingDAO,mailSender);
+
+        int actual = bookingService1.getAvailablePlaceCount();
+        assertEquals(18, actual);
     }
 }
