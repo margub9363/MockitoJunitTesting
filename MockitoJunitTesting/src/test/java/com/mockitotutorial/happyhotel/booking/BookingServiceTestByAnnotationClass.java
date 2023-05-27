d6596d1eb7480914c0eae6680bef66d9f8790f59
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-27 16:07:47 +0530

    Making Use of Annotations

---
+++ MockitoJunitTesting/src/test/java/com/mockitotutorial/happyhotel/booking/BookingServiceTestByAnnotationClass.java
@@ -0,0 +1,99 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookingServiceTestByAnnotationClass {
+
+    @InjectMocks
+    private BookingService bookingService;
+    @Mock
+    private PaymentService paymentServiceMock;
+    @Mock
+    private RoomService roomServiceMock;
+    @Mock
+    private BookingDAO bookingDAOMock;
+    @Mock
+    private MailSender mailSenderMock;
+
+    @Test
+    public void calculatePrice(){
+
+//        given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020,01,01),LocalDate.of(2020,01,05),2,false);
+        double expected = 4 * 2 * 50.0;
+//        when
+        double actual = bookingService.calculatePrice(bookingRequest);
+//        then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void getAvailablePlaceCount_test() {
+
+//        since in line no 27 we are mocking roomserive thats why we are givinng values below. If we would have used actual class then we should have not required to pass values below
+//        given
+        List<Room> availableRooms = new ArrayList<>();
+        availableRooms.add(new Room("1",1));
+        availableRooms.add(new Room("2",2));
+        availableRooms.add(new Room("3",3));
+        when(roomServiceMock.getAvailableRooms()).thenReturn(availableRooms);
+//        when
+        int actualValue = bookingService.getAvailablePlaceCount();
+        int expectedValue = 6;
+//        then
+        assertEquals(actualValue,expectedValue);
+    }
+
+    @Test
+    void getAvailablePlaceCount_MultipleReturnValues_whenCalledMultipleTimes() {
+
+//        since in line no 27 we are mocking roomserive thats why we are givinng values below. If we would have used actual class then we should have not required to pass values below
+//        given
+        List<Room> availableRooms = new ArrayList<>();
+        availableRooms.add(new Room("1",1));
+     List<Room> availableRoomOnSecondCall = new ArrayList<>();
+        availableRoomOnSecondCall.add(new Room("2",2));
+        availableRoomOnSecondCall.add(new Room("3",3));
+        List<Room> availableRoomOnThirdCall = new ArrayList<>();
+        availableRoomOnThirdCall.add(new Room("4",4));
+        availableRoomOnThirdCall.add(new Room("5",5));
+        when(roomServiceMock.getAvailableRooms()).thenReturn(availableRooms).thenReturn(availableRoomOnSecondCall).thenReturn(availableRoomOnThirdCall);
+//        when
+        int actualFirst = bookingService.getAvailablePlaceCount();
+        int actualSecond = bookingService.getAvailablePlaceCount();
+        int actualThird = bookingService.getAvailablePlaceCount();
+
+//        then
+        assertEquals(actualFirst,1);
+        assertEquals(actualSecond,5);
+        assertEquals(actualThird,9);
+    }
+
+    @Test
+    void makeBooking_shouldThrowExceptionWhenNoRoomAvailable() {
+//        given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020,01,01),LocalDate.of(2020,01,05),2,false);
+        when(roomServiceMock.findAvailableRoomId(bookingRequest)).thenThrow(new BusinessException());
+
+//        when
+        Executable executable = () -> bookingService.makeBooking(bookingRequest);
+//        then
+        assertThrows(BusinessException.class,executable);
+    }
+}
